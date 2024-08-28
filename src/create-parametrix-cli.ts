52cdd2076d6b5dd1857a3d5dcb83812e9ab67d03
charlyoleg <charles.braquet@gmx.fr>
2024-08-28 06:26:38 +0200

    complete ci

--- src/create-parametrix-cli.ts
+++ src/create-parametrix-cli.ts
@@ -1,67 +1,57 @@
 #!/usr/bin/env node
 // create-parametrix-cli.ts
 
-import {
-  intro,
-  outro,
-  confirm,
-  select,
-  spinner,
-  isCancel,
-  cancel,
-  text,
-} from '@clack/prompts';
+import { intro, outro, confirm, select, spinner, isCancel, cancel, text } from '@clack/prompts';
 import { setTimeout as sleep } from 'node:timers/promises';
 import color from 'picocolors';
 
 async function main() {
-  console.log();
-  intro(color.inverse(' create-my-app '));
+	console.log();
+	intro(color.inverse(' create-my-app '));
 
-  const name = await text({
-    message: 'What is your name?',
-    placeholder: 'Anonymous',
-  });
+	const name = await text({
+		message: 'What is your name?',
+		placeholder: 'Anonymous'
+	});
 
-  if (isCancel(name)) {
-    cancel('Operation cancelled');
-    return process.exit(0);
-  }
+	if (isCancel(name)) {
+		cancel('Operation cancelled');
+		return process.exit(0);
+	}
 
-  const shouldContinue = await confirm({
-    message: 'Do you want to continue?',
-  });
+	const shouldContinue = await confirm({
+		message: 'Do you want to continue?'
+	});
 
-  if (isCancel(shouldContinue)) {
-    cancel('Operation cancelled');
-    return process.exit(0);
-  }
+	if (isCancel(shouldContinue)) {
+		cancel('Operation cancelled');
+		return process.exit(0);
+	}
 
-  const projectType = await select({
-    message: 'Pick a project type.',
-    options: [
-      { value: 'ts', label: 'TypeScript' },
-      { value: 'js', label: 'JavaScript' },
-      { value: 'coffee', label: 'CoffeeScript', hint: 'oh no' },
-    ],
-  });
+	const projectType = await select({
+		message: 'Pick a project type.',
+		options: [
+			{ value: 'ts', label: 'TypeScript' },
+			{ value: 'js', label: 'JavaScript' },
+			{ value: 'coffee', label: 'CoffeeScript', hint: 'oh no' }
+		]
+	});
 
-  if (isCancel(projectType)) {
-    cancel('Operation cancelled');
-    return process.exit(0);
-  }
+	if (isCancel(projectType)) {
+		cancel('Operation cancelled');
+		return process.exit(0);
+	}
 
-  const s = spinner();
-  s.start('Installing via npm');
+	const s = spinner();
+	s.start('Installing via npm');
 
-  await sleep(3000);
+	await sleep(3000);
 
-  s.stop('Installed via npm');
+	s.stop('Installed via npm');
 
-  outro("You're all set!");
+	outro("You're all set!");
 
-  await sleep(1000);
+	await sleep(1000);
 }
 
 main().catch(console.error);
-
